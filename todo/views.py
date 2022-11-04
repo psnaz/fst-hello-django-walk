072638fbb05f729939be5b5dcabe06b943b22304
psnaz <psnazle@gmail.com>
2022-11-04 11:32:41 +0000

    Add toggle functionality

--- todo/views.py
+++ todo/views.py
@@ -27,7 +27,7 @@ def add_item(request):
 def edit_item(request, item_id):
     item = get_object_or_404(Item, id=item_id)
     if request.method == 'POST':
-        form=ItemForm(request.POST, instance=item)
+        form = ItemForm(request.POST, instance=item)
         if form.is_valid():
             form.save()
             return redirect('get_todo_list')
@@ -36,3 +36,10 @@ def edit_item(request, item_id):
         'form': form
     }
     return render(request, 'todo/edit_item.html', context)
+
+
+def toggle_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.done = not item.done
+    item.save()
+    return redirect('get_todo_list')
