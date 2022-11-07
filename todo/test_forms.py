d30f9a18fda4fd6b27d967db0abe90e764c38ab5
psnaz <psnazle@gmail.com>
2022-11-07 11:08:50 +0000

    Test views

--- todo/test_forms.py
+++ todo/test_forms.py
@@ -17,5 +17,3 @@ class TestItemForm(TestCase):
     def test_fields_are_explicit_in_form_metaclass(self):
         form = ItemForm()
         self.assertEqual(form.Meta.fields, ['name', 'done'])
-
-
