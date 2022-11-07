709ac9a97fb4e9be49e12551557283e4d3c80bdb
psnaz <psnazle@gmail.com>
2022-11-07 13:45:51 +0000

    Prepare to deploy to heroku

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 
 if os.path.exists("env.py"):
     import env
@@ -81,11 +82,15 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://duyinxdtlejlgu:19435e8ff0ac10d9beb628eb3beda652eedc5242da9d048b38d80115d9d98a0c@ec2-63-34-180-86.eu-west-1.compute.amazonaws.com:5432/d6o23rqhsbian2')
 }
 
 
